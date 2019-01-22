143d73315dafcccae96413ebe8e8d7f9decac9b6
royboy777 <roycanaf@hotmail.com>
2019-01-22 08:48:24 +0200

    Solution to Network issue of exercise1

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-#add fix to exercise1 here
+#fix to problem where curl resulted in failure - curl: (7) couldn't connect to host
+#this was due to networking issue - the request didn't reach the router/vagrant host to the external network
+#but stayed in the private network 192.168.x.x
+#the solution was to refer it to the gw with the ip of router/vagrant host
+sudo route del -net 208.86.224.90 netmask 255.255.255.255 gw 0.0.0.0
+sudo route add -net 208.86.224.90 netmask 255.255.255.255 gw 10.0.2.2

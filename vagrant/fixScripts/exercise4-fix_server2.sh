3b17c89e4ba3495462b17d1344e830c2080cb21a
royboy777 <roycanaf@hotmail.com>
2019-01-22 08:51:49 +0200

    Solution server1 hostname resolution issue of exercise4-server2

--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-#add fix to exercise4-server2 here
+#solution to the problem where ssh command to server1failed 
+#problem was that the hostname server1 was not resolved (didn't appear in any resolution file)
+#the solution is to add it to /etc/hosts file in case it does not exist
+if ! ( grep -lq '192.168.100.10' /etc/hosts ) ; then sudo sed -i "\$a192.168.100.10 server1 Server1" /etc/hosts >> /dev/null ; fi

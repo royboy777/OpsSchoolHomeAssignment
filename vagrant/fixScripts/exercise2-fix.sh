7eff9f3af71618eded08f8dc566f7e3a5fb2be29
royboy777 <roycanaf@hotmail.com>
2019-01-22 08:49:00 +0200

    Solution to DNS resolution issue of exercise2

--- vagrant/fixScripts/exercise2-fix.sh
+++ vagrant/fixScripts/exercise2-fix.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-#add fix to exercise2 here
+#solution to the problem where curl command failed with http 403 error - forbidden
+#problem was that the hostname/domain name was in /etc/hosts and referred to localhost
+#which actually got to the local apache where the file does not exists ascii/ab/007.txt
+#the solution is to delete the entry from /etc/hosts so it will perform correct dns resolution
+sudo sed -i '/www.ascii-art.de/d' /etc/hosts

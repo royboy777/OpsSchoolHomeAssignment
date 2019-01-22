f0bf85fb0d87b641b19796f09215cba867f1bc39
royboy777 <roycanaf@hotmail.com>
2019-01-22 08:52:57 +0200

    Solution of Password-Less Ssh over Server1 , Resolution of Exercise5-server1

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-#add fix to exercise5-server1 here
+#Solution to the issue of Password-less ssh implemented on server1 in order to ssh server2 without password
+#This is by implementing procedure for that , including : generating key in case not exist already, chmod to authorized_keys
+ssh-keygen -t rsa -N "" -f /home/vagrant/.ssh/id_rsa
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh/
+chmod 600 ~/.ssh/authorized_keys
